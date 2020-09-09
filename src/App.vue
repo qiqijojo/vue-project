a1919e7f605be13121bf46c5e66db1f9d5f448d9
jojo <774948439@qq.com>
2020-09-09 16:05:41 +0800

    使用常规写法-创建页面组件及子组件

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,59 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <p>{{msg}}</p>
+    <button @click="say">我是按钮</button>
+    <One />
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
   </div>
 </template>
+<script>
+import One from './components/One'
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  name: 'JOJO',
+  data: () => {
+    return {
+      msg: 'jojo'
     }
+  },
+  methods: {
+    say () {
+      console.log('0000', 'say')
+    }
+  },
+  components: {
+    One
   }
 }
+</script>
+
+<style lang="scss">
+p {
+  background: #f00;
+  font-size: 20px;
+}
+// #app {
+//   font-family: Avenir, Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+// }
+
+// #nav {
+//   padding: 30px;
+
+//   a {
+//     font-weight: bold;
+//     color: #2c3e50;
+
+//     &.router-link-exact-active {
+//       color: #42b983;
+//     }
+//   }
+// }
 </style>
---
+++ src/components/One.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>我是第一个子组件</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'One'
+}
+</script>
+<style lang="scss" scoped>
+p{
+  background: #eeeeee;
+}
+
+</style>
