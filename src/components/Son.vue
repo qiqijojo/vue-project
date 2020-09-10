7fcfa02a4a1c89649861a130b8721ba3e6683988
jojo <774948439@qq.com>
2020-09-10 16:06:25 +0800

    组件：v-slot指令

--- src/components/Son.vue
+++ src/components/Son.vue
@@ -4,10 +4,26 @@
     <div>{{parentName}}</div>
     <button @click="sonFunc">我是按钮</button>
     <ChildCom :childName="parentName" @childSay="sonFunc" ref="child">
-      <div slot="one">我想多加点内容----1</div>
+
+      <!-- <div slot="one">我想多加点内容----1</div>
       <div slot="two">我想多加点内容----2</div>
       <div slot="one">我想多加点内容----1</div>
-      <div slot="two">我想多加点内容----2</div>
+      <div slot="two">我想多加点内容----2</div> -->
+
+      <!-- <template v-slot:one>
+        <div>我想多加点内容----1</div>
+      </template>
+      <template v-slot:two>
+        <div>我想多加点内容----2</div>
+      </template> -->
+
+      <template #one>
+        <div>我想多加点内容----1</div>
+      </template>
+      <template #two>
+        <div>我想多加点内容----2</div>
+      </template>
+
     </ChildCom>
   </div>
 </template>
