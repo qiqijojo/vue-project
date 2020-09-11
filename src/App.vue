8d394bd912a96b0de961a2c87977402d146c6ed2
jojo <774948439@qq.com>
2020-09-11 16:28:56 +0800

    watch属性监听数据变化：执行相应的方法

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,17 @@
     <router-view name='name1' />
     <router-view name='name2' /> -->
 
-    <input v-model="num1" @keyup="changeNum1" />
+    <!-- <input v-model="num1" @keyup="changeNum1" />
     <span> + </span>
     <input v-model="num2" @keyup="changeNum2" />
     <span> = </span>
-    <input v-model="sum" disabled />
+    <input v-model="sum" disabled /> -->
 
+     <input v-model="num1" />
+    <span> + </span>
+    <input v-model="num2" />
+    <span> = </span>
+    <input v-model="sum" disabled />
   </div>
 </template>
 <script>
@@ -33,6 +38,16 @@ export default {
       num1: 0,
       num2: 0,
       sum: 0
+    }
+  },
+  watch: {
+    num1 (newValue, oldValue) {
+      console.log(newValue, oldValue)
+      this.sum = parseInt(this.num1 || 0) + parseInt(this.num2 || 0)
+    },
+    num2 (newValue, oldValue) {
+      console.log(newValue, oldValue)
+      this.sum = parseInt(this.num1 || 0) + parseInt(this.num2 || 0)
     }
   },
   methods: {
