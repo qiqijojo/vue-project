c907e807edee893340732143a3ebefd7ab7ddbea
jojo <774948439@qq.com>
2020-09-11 16:21:17 +0800

    键盘keyup监听数据变化：实现加法

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <!-- <One @changeOne="changeNum" />
-    <Two :numTwo="num" /> -->
+    <Two :numTwo="num" />
     App----{{num}}----{{$store.state.name}}
 
     <div>{{format}}</div>
@@ -9,7 +9,14 @@
     <router-link to='/one?name=jojo&age=25' tag='button'>我是One</router-link>
     <router-link to='/two/11111/222222' tag='button'>我是Two</router-link>
     <router-view name='name1' />
-    <router-view name='name2' />
+    <router-view name='name2' /> -->
+
+    <input v-model="num1" @keyup="changeNum1" />
+    <span> + </span>
+    <input v-model="num2" @keyup="changeNum2" />
+    <span> = </span>
+    <input v-model="sum" disabled />
+
   </div>
 </template>
 <script>
@@ -22,13 +29,23 @@ export default {
   data: () => {
     return {
       num: 0,
-      myMsg: 'English'
+      myMsg: 'English',
+      num1: 0,
+      num2: 0,
+      sum: 0
     }
   },
   methods: {
     changeNum (data) {
       console.log('00', data)
       this.num = data
+    },
+    changeNum1 () {
+      console.log('111')
+      this.sum = parseInt(this.num1 || 0) + parseInt(this.num2 || 0)
+    },
+    changeNum2 () {
+      this.sum = parseInt(this.num1 || 0) + parseInt(this.num2 || 0)
     }
   },
   computed: {
