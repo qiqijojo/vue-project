d3714a630e7bd12696f71fc08799b2e7a08bbc78
jojo <774948439@qq.com>
2020-09-15 15:56:04 +0800

    element-ui：组件按需加载

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
+import { Row, Button, Switch } from 'element-ui'
 
-Vue.use(ElementUI)
+// Vue.use(ElementUI)
+Vue.use(Row)
+Vue.use(Button)
+Vue.use(Switch)
 
 // Vue.config.productionTip = false
 
